823eaca2aed0c9fe7a34ac6b768d00c816ac3039
AchroDev <achrodev@proton.me>
2024-09-25 22:06:17 -0500

    Added includes and size of string example

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <string>
+#include <cstring>
+
+#include <stdlib.h>
 
 // String Literals in C++ are a series of characters between two double quotes ""
 
 int main()
 {
-    "AchroDev"; // This is a definition of a string literal // (const char[9])"AchroDev". It includes the null termination char
+    const char name[10] = "Achr\0oDev";     // This is a definition of a string literal // (const char[9])"AchroDev". It includes the null termination char
+    std::cout << strlen(name) << std::endl; // only prints 4 as we manually put a null termination character after the fourth index
     std::cin.get();
 }
