ef6c8f2f52e484d45a97ac5aae39e844c2219e93
AchroDev <achrodev@proton.me>
2024-09-26 18:52:47 -0500

    Refactored code

--- main.cpp
+++ main.cpp
@@ -8,8 +8,7 @@
 
 int main()
 {
-    char *name = "AchroDev";                // A string literal can also be assigned to a char *. Removing const with some compilers will let you compile this
-    name[2] = 'a';                          // You can see this isn't allowed
-    std::cout << strlen(name) << std::endl; // now prints the actual size of the const char array
+    char *name = "AchroDev"; // A string literal can also be assigned to a char *. Removing const with some compilers will let you compile this
+    std::cout << name << std::endl;
     std::cin.get();
 }
