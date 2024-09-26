ba9793dfc02dbcce9427ebc287438cf613f3eb13
AchroDev <achrodev@proton.me>
2024-09-26 18:46:30 -0500

    Switched to a const char * and added comments

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@
 
 int main()
 {
-    const char name[9] = "AchroDev";        // This is a definition of a string literal // (const char[9])"AchroDev". It includes the null termination char
+    const char *name = "AchroDev";          // A string literal can also be assigned to a const char * (pointer) // remember const means no manipulation
+    name[2] = 'a';                          // You can see this isn't allowed
     std::cout << strlen(name) << std::endl; // now prints the actual size of the const char array
     std::cin.get();
 }
