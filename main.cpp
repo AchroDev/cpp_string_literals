578449bce2f85608e3780a34d36f6e9e31e9895d
AchroDev <achrodev@proton.me>
2024-09-26 18:49:14 -0500

    Removed const to compile into assembly and view instructions

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 
 int main()
 {
-    const char *name = "AchroDev";          // A string literal can also be assigned to a const char * (pointer) // remember const means no manipulation
+    char *name = "AchroDev";                // A string literal can also be assigned to a char *. Removing const with some compilers will let you compile this
     name[2] = 'a';                          // You can see this isn't allowed
     std::cout << strlen(name) << std::endl; // now prints the actual size of the const char array
     std::cin.get();
