e50f2198522cb78d9ee0d7f42e3a1a36626cf2cf
AchroDev <achrodev@proton.me>
2024-09-25 21:59:50 -0500

    Added string literal example

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <string>
+
+// String Literals in C++ are a series of characters between two double quotes ""
 
 int main()
 {
-
+    "AchroDev"; // This is a definition of a string literal // (const char[9])"AchroDev". It includes the null termination char
     std::cin.get();
 }
