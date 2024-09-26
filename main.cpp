9d1c8c1dad3353575cd871ab1073e0609d3748fb
AchroDev <achrodev@proton.me>
2024-09-25 22:35:49 -0500

    Removed the null term char

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 
 int main()
 {
-    const char name[10] = "Achr\0oDev";     // This is a definition of a string literal // (const char[9])"AchroDev". It includes the null termination char
-    std::cout << strlen(name) << std::endl; // only prints 4 as we manually put a null termination character after the fourth index
+    const char name[9] = "AchroDev";        // This is a definition of a string literal // (const char[9])"AchroDev". It includes the null termination char
+    std::cout << strlen(name) << std::endl; // now prints the actual size of the const char array
     std::cin.get();
 }
