b45f0f7aa15b44e58dbfa6e2a21acefc1af8ef9c
AchroDev <achrodev@proton.me>
2024-09-26 19:18:34 -0500

    Added other string type examples with std::string_literals

--- main.cpp
+++ main.cpp
@@ -8,11 +8,21 @@
 
 int main()
 {
+    // Introduced in C++ 14
+    using namespace std::string_literals;
+
+    // With std::string_literals you have some extra tools for strings
+    std::string name0 = "AchroDev"s + " hello";       // Adding 's' to the end of a string signifies returning a std::string, this allows appending another string
+    std::wstring name01 = L"AchroDev"s + L" hello";   // A wide string is written similarly and still requires the 'L' for both strings
+    std::u16string name02 = u"AchroDev"s + u" hello"; // Same for the 16-bit string and 'u'
+    std::u32string name03 = U"AchroDev"s + U" hello"; // and same for the 32-bit string and 'U'
+
     const char *name = u8"AchroDev";    // Remember char is a 1 byte type, can also be force defined by appended 'u8'
     const wchar_t *name2 = L"AchroDev"; // wchar_t is a wide character. The string must be appended with 'L' to signify the following string is made up of wide chars
+                                        // wchar is also 2 bytes per char on Windows, 4 bytes on Linux
 
-    const char16_t *name3 = u"AchroDev"; // Introduced in C++ 11, unsigned 16-bit
-    const char32_t *name4 = U"AchroDev"; // Introduced in C++ 11, unsigned 32-bit
+    const char16_t *name3 = u"AchroDev"; // Introduced in C++ 11, 2 bytes per char, unsigned 16-bit
+    const char32_t *name4 = U"AchroDev"; // Introduced in C++ 11, 4 bytes per char, unsigned 32-bit
 
     std::cout << name << std::endl;
     std::cin.get();
